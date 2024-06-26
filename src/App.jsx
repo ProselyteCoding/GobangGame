82844c7aceb04f326d711c115eb2e7a02df1133d
Proselyte <141432318+ProselyteCoding@users.noreply.github.com>
2024-06-26 16:41:40 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Board from "./components/Board.jsx"
+import "./App.css";
+
+function App()
+{
+    return(
+        <>
+            <h1 className="head" >CHESS GAME</h1>
+            <Board/> 
+        </>
+    );
+}
+export default App;
